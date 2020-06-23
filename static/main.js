0fc6a48c2113d9268c71b3e9f6b27482d92c228a
Evans Muzulu <emuzulu@oberlin.edu>
2020-06-23 01:47:24 -0400

    Current Progress

--- static/main.js
+++ static/main.js
@@ -1,14 +1,11 @@
-alert("hello");
-
+function post(){
+    let company = document.querySelector("#compamy").value;
+    let role = document.querySelector("#role").value;
+    alert(company + " for the role" + role);
+}
 
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("#add").onclick = post;
 }
 );
 
-
-function post(){
-    let company = document.querySelector("#compamy").value;
-    let role = document.querySelector("#role").value;
-    alert(company + " for the role" + role);
-}
